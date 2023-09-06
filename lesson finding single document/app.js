b8f6fce73fdc2c85056fbccd89b538a29cfef917
Justine Edward Santos <hydxenonmain@gmail.com>
2023-09-06 13:05:42 +0800

    Add Lesson Delete Request

--- lesson finding single document/app.js
+++ lesson finding single document/app.js
@@ -44,3 +44,19 @@ app.get("/books/:id", (req, res) => {
     res.status(500).json({error: "Not a valid document ID"})
   }
 });
+
+
+app.delete('/books/:id', (req, res)=>{
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .deleteOne({ _id: new ObjectId(req.params.id) })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could not delete the document" });
+      });
+  } else{
+    res.status(500).json({error: "Not a valid document ID"})
+  }
+})
